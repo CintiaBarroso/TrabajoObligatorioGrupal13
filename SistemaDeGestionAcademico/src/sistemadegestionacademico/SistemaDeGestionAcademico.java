1b0b3f1cb9eb298913494ed3bdca8dbb38a5d890
FacundoBacaicoa <facundobacaicoa@gmail.com>
2024-05-15 15:44:10 -0300

    trabajo actualizado

--- SistemaDeGestionAcademico/src/sistemadegestionacademico/SistemaDeGestionAcademico.java
+++ SistemaDeGestionAcademico/src/sistemadegestionacademico/SistemaDeGestionAcademico.java
@@ -4,17 +4,45 @@
  */
 package sistemadegestionacademico;
 
-/**
- *
- * @author cinti
- */
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
 public class SistemaDeGestionAcademico {
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
     
-}
+    public static void main(String[] args) {
+         try (Connection conn = Conexion.getConexion()) {
+            System.out.println("Conexión establecida con éxito.");
+
+            // Insertar 3 empleados
+            insertarAlumno(conn, "alumno 1");
+            insertarAlumno(conn, "alumno 2");
+            insertarAlumno(conn, "alumno 3");
+
+            // Insertar 2 herramientas
+            insertarMateria(conn, "materia 1", 15);
+            insertarMateria(conn, "materia 2", 20);
+            
+            
+            //Insertar inscripciones
+            
+            
+            // Listar todas las herramientas con stock superior a 10
+            listarHerramientas(conn);
+
+            // Dar de baja al primer empleado ingresado a la base de datos
+            darDeBajaPrimerEmpleado(conn);
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("No se encontró el driver de MariaDB: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Error de conexión: " + e.getMessage());
+        }
+    }
+        
+    }
+
+    
+
